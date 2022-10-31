4d210eaeb9fe964412af6bc3e1700d34788ca902
zoza <97721931+zoza21@users.noreply.github.com>
2022-10-31 18:05:19 +0800

    Update models.py

--- todolistapp/listings/models.py
+++ todolistapp/listings/models.py
@@ -3,13 +3,6 @@ from django.utils import timezone
 
 
 # Create your models here.
-
-	
-	#this creates clas, band, inherits from modelsModel (djangos model base class)
-	#add class attribute to the name. assigns CharField (which is short for character field)
-	#this field will sotre characer/text/string data
-	#max lengt set to 100
-	#django framework creates constructor for you don't need to do __init__ like python class
 	
 
 class Listing(models.Model):
